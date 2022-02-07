86166ddd2dc880b358643b77dbb51ebfd5893619
dhorning0028 <dustin.horning@kellogg.northwestern.edu>
2022-02-07 00:51:25 +0000

    arghhhhh i dont understand loops :((((

--- kmdb.rb
+++ kmdb.rb
@@ -174,28 +174,85 @@ dark_knight_rises.save
 puts "There are #{Movie.all.count} movies"
 
 # Adding roles to roles table
-
-
 role1 = Role.new
 # role1.movie_id = movie.id
 role1.person_id = person1.id
 role1.character_name = "Bruce Wayne"
 role1.save
 
+role2 = Role.new
+# role2.movie_id = movie.id
+role2.person_id = person2.id
+role2.character_name = "Selina Kyle"
+role2.save
+
+role3 = Role.new
+# role3.movie_id = movie.id
+role3.person_id = person3.id
+role3.character_name = "Alfred"
+role3.save
+
+role4 = Role.new
+# role4.movie_id = movie.id
+role4.person_id = person4.id
+role4.character_name = "Ra's Al Ghul"
+role4.save
+
+role5 = Role.new
+# role5.movie_id = movie.id
+role5.person_id = person5.id
+role5.character_name = "Commissioner Gordon"
+role5.save
+
+role6 = Role.new
+# role6.movie_id = movie.id
+role6.person_id = person6.id
+role6.character_name = "Joker"
+role6.save
+
+role7 = Role.new
+# role7.movie_id = movie.id
+role7.person_id = person7.id
+role7.character_name = "Harvey Dent"
+role7.save
+
+role8 = Role.new
+# role8.movie_id = movie.id
+role8.person_id = person8.id
+role8.character_name = "Rachel Dawes"
+role8.save
+
+role9 = Role.new
+# role9.movie_id = movie.id
+role9.person_id = person9.id
+role9.character_name = "Bane"
+role9.save
+
+role10 = Role.new
+# role10.movie_id = movie.id
+role10.person_id = person10.id
+role10.character_name = "John Blake"
+role10.save
+
+role11 = Role.new
+# role11.movie_id = movie.id
+role11.person_id = person11.id
+role11.character_name = "Director"
+role11.save
+
 # variables
 movie = Movie.where({ title: "The Dark Knight Rises"})[0]
 puts movie.title
 
 christopher_nolan = Person.where({ name: "Christopher Nolan" })[0]
-puts christopher_nolan.title
+puts christopher_nolan.name
 
 # Loops
-movie = Movie.where({ person_id: christopher_nolan.id })
-puts movie
+movies = Movie.where({ person_id: role.id })
+puts movie.title
 
-puts "Movies: #{movie.count}"
+puts "Movies: #{movies.count}"
 for movie in movies
     puts "#{movie.title} #{(movie.year)}"
 end
 
-movies = christopher_nolan.movie
